145b2e897b388303dda0aeb84e74ebb8127b87b0
jorgeescobedo117 <jorgeescobedo117@gmail.com>
2025-05-28 15:42:37 -0600

    SE modifico el texto de suma.py

--- suma.py
+++ suma.py
@@ -1,5 +1,5 @@
 def suma(*num):
     return sum(num)
 print(suma(2,3,4,5,2,5,6,2,5,10))
-print("HOLA , ESTA ESTA EN LA RAMA MODIFICAR TEXTO "+"MODIFICADO EN LA RAMA MODIFICAR-TEXTO")
+print("HOLA , ESTA ESTA modificado por jorgeescobedo")
 
