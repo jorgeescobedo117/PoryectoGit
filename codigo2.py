5157f0c6d4046996a4d8be89b787127732a390ff
jorgeescobedo117 <jorgeescobedo117@gmail.com>
2025-05-28 17:46:17 -0600

    Añadido en la rama master

--- codigo2.py
+++ codigo2.py
@@ -1,7 +1,8 @@
-nombre="JOrge"
-apellidop="escobedo"
-apellidom="Medina"
-apodo ="Merok"
+nombre="JOrge" ##Esta funcion muestra tu nombre
+apellidop="escobedo"##Esta funcion muestra tu apellido
+apellidom="Medina"##Esta funcion muestra tu nombre
+apodo ="Merok"##Esta funcion muestra tu nombre
 
 
-print("hola"+ apellidop+apellidom+apodo)
+print("hola"+ apellidop+apellidom+apodo) ##SE MANDA A IMPRIMIR UN HOLA MAS LAS 
+                                         ##FUNCIONES
