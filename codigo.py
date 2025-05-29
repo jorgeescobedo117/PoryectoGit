c83b7303f3503f34191aa6d6864a335b55e79704
jorgeescobedo117 <150106797+jorgeescobedo117@users.noreply.github.com>
2025-05-28 18:14:04 -0600

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -2,7 +2,7 @@ nombre="JOrge" ##Esta funcion muestra tu nombre
 apellidop="escobedo"##Esta funcion muestra tu apellido
 apellidom="Medina"##Esta funcion muestra tu nombre
 apodo ="Merok"##Esta funcion muestra tu nombre
-
+mainsaxd
 
 print("hola"+ apellidop+apellidom+apodo) ##SE MANDA A IMPRIMIR UN HOLA MAS LAS 
                                          ##FUNCIONES
